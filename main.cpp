30b096fadd62389b48668a84d27d36e5dfb263a4
tubesteer <praditdimas1@gmail.com>
2026-05-27 21:55:38 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,43 @@
 #include <mod/amlmod.h>
 #include <mod/logger.h>
-#include <mod/config.h>
 
-// Do not include all like a crazy! It will slowdown the mod loading!
-#include <aml-psdk/game_sa/base/Timer.h>
+// Informasi Mod kamu (Ganti sesuai keinginan)
+MYMOD(com.dimas.visual64, VisualSADox64, 1.0, Dimas)
 
-MYMODCFG(net.psdk.mymod.guid, AML PSDK Template, 1.0, Author)
-//MYMOD(net.psdk.mymod.guid, AML PSDK Template NoConfig, 1.0, Author)
-//NEEDGAME(net.psdk.mygame)
-//BEGIN_DEPLIST()
-//    ADD_DEPENDENCY_VER(net.psdk.aml, 1.0)
-//END_DEPLIST()
+// Alamat dasar dari library game
+uintptr_t pGameAddress = 0;
 
-DECL_HOOKv(CTimer__Update)
+extern "C" void OnModLoad()
 {
-    CTimer__Update();
-    CTimer::m_snTimeInMilliseconds += 5;
-    logger->Info("CTimer__Update: %u", CTimer::m_snTimeInMilliseconds);
-    CTimer::m_snTimeInMilliseconds -= 5;
-}
+    logger->Info("--- Memulai Pemasangan VisualSADox 64-bit ---");
 
-ON_MOD_LOAD()
-{
-    logger->SetTag("PSDK Template");
-    
-    HOOK(CTimer__Update, CTimer::Update); // just an example!
+    // 1. Mengunci library utama GTA SA 2.10 (arm64-v8a)
+    pGameAddress = aml->GetLib("libGTASA.so");
+
+    if (pGameAddress)
+    {
+        logger->Info("libGTASA.so terdeteksi di memori.");
+
+        /* 
+           2. MODIFIKASI JARAK PANDANG & KABUT (FOG)
+           Di versi 2.10 64-bit, variabel global untuk mengatur jarak kabut 
+           dan draw distance berada di sekitar offset ini.
+           Kita overwrite nilainya dengan angka float yang lebih besar.
+        */
+        
+        // Contoh memasukkan nilai Jarak Jauh (Draw Distance) sebesar 2000.0f
+        // (Default game biasanya hanya sekitar 800.0f - 1200.0f)
+        uintptr_t offsetDrawDist = pGameAddress + 0x9B4D20; // Offset ilustrasi v2.10 arm64
+        aml->WriteFloat(offsetDrawDist, 2000.0f);
+        
+        // Memaksa nilai ketebalan kabut (Fog) menjadi sangat tipis / 0
+        uintptr_t offsetFogDensity = pGameAddress + 0x9B4D24; 
+        aml->WriteFloat(offsetFogDensity, 0.0f);
+
+        logger->Info("Fitur No-Fog & Max Draw Distance berhasil diterapkan!");
+    }
+    else
+    {
+        logger->Error("Gagal memuat libGTASA.so! Pastikan game berjalan.");
+    }
 }
