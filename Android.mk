f8b3235465dd083b4c88cecce411f2c2b1206fd3
tubesteer <praditdimas1@gmail.com>
2026-05-27 22:03:06 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,13 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
+# Menentukan ekstensi file C++ yang digunakan
 LOCAL_CPP_EXTENSION := .cpp .cc
-ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-    LOCAL_MODULE := AML_PSDK_Template
-else
-    LOCAL_MODULE := AML_PSDK_Template64
-endif
+
+# Nama file .so hasil jadi (Nanti jadinya: libVisualSADox64.so)
+LOCAL_MODULE := VisualSADox64
+
+# Pastikan file-file ini ada di dalam folder jni kamu
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
-LOCAL_CXXFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
+
+# Flag untuk optimasi kode (Menghapus parameter softfp agar aman di 64-bit)
+LOCAL_CXXFLAGS += -O3 -DNDEBUG -std=c++17
 LOCAL_LDLIBS += -llog
+
 include $(BUILD_SHARED_LIBRARY)
